2cac1e7200a563b67ac74772848fef03b7db8e53
Jamy54 <kishor0907054@gmail.com>
2022-04-24 16:06:07 -0500

    all with configuration

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from torchvision.utils import save_image
 from PIL import Image
 import torch.optim as optim
 import torchvision
+import sys
+import os
 import torch.nn.functional as F
 
 
@@ -42,79 +44,94 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR100(root='./data', train=True,
-                                        download=True, transform=transform)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=1,
-                                          shuffle=True, num_workers=2)
 
 def load_image(image_name):
     image = Image.open(image_name)
     image = loader(image).unsqueeze(0)
     return image.to(device)
 
-style_img=load_image("Style.jpg")
 
+count = 0
+style_img = load_image("Style.jpg")
 model = VGG().to(device).eval()
-
-total_steps = 100
+total_steps = 200
 learning_rate = 0.001
 alpha = 1
 beta = 0.01
 
+def perform_style(images,outputLocation):
+    global count
+    original_img = [transforms.Resize(size=size)(images) for size in (1, 3, 356)][2]
+    generated = original_img.clone().requires_grad_(True)
 
-count=0
+    # original_img = load_image("Original.png")
+    # generated = original_img.clone().requires_grad_(True)
 
+    optimizer = optim.Adam([generated], lr=learning_rate)
 
-if __name__ ==  '__main__':
+    for step in range(total_steps):
+        # Obtain the convolution features in specifically chosen layers
+        generated_features = model(generated)
+        original_img_features = model(original_img)
+        style_features = model(style_img)
 
+        # Loss is 0 initially
+        style_loss = original_loss = 0
+
+        # iterate through all the features for the chosen layers
+        for gen_feature, orig_feature, style_feature in zip(
+                generated_features, original_img_features, style_features
+        ):
+            # batch_size will just be 1
+            batch_size, channel, height, width = gen_feature.shape
+            original_loss += torch.mean((gen_feature - orig_feature) ** 2)
+            # Compute Gram Matrix of generated
+            G = gen_feature.view(channel, height * width).mm(
+                gen_feature.view(channel, height * width).t()
+            )
+            # Compute Gram Matrix of Style
+            A = style_feature.view(channel, height * width).mm(
+                style_feature.view(channel, height * width).t()
+            )
+            style_loss += torch.mean((G - A) ** 2)
+
+        total_loss = alpha * original_loss + beta * style_loss
+        optimizer.zero_grad()
+        total_loss.backward()
+        optimizer.step()
+
+    print(total_loss)
+    if not os.path.isdir(outputLocation):
+        os.mkdir(outputLocation)
+    name = outputLocation + str(count) + "generated.png"
+    save_image(generated, name)
+    name = outputLocation + str(count) + "original.png"
+    save_image(original_img, name)
+    count += 1
+
+def perform_styles(trainloader,outputLocation):
     for i, data in enumerate(trainloader, 0):
         images, labels = data
         images = images.cuda()
+        perform_style(images,outputLocation)
 
-        original_img = [transforms.Resize(size=size)(images) for size in (1, 3, 356)][2]
-        generated = original_img.clone().requires_grad_(True)
 
-        #original_img = load_image("Original.png")
-        #generated = original_img.clone().requires_grad_(True)
 
-        optimizer = optim.Adam([generated], lr=learning_rate)
-
-        for step in range(total_steps):
-            # Obtain the convolution features in specifically chosen layers
-            generated_features = model(generated)
-            original_img_features = model(original_img)
-            style_features = model(style_img)
-
-            # Loss is 0 initially
-            style_loss = original_loss = 0
-
-            # iterate through all the features for the chosen layers
-            for gen_feature, orig_feature, style_feature in zip(
-                    generated_features, original_img_features, style_features
-            ):
-                # batch_size will just be 1
-                batch_size, channel, height, width = gen_feature.shape
-                original_loss += torch.mean((gen_feature - orig_feature) ** 2)
-                # Compute Gram Matrix of generated
-                G = gen_feature.view(channel, height * width).mm(
-                    gen_feature.view(channel, height * width).t()
-                )
-                # Compute Gram Matrix of Style
-                A = style_feature.view(channel, height * width).mm(
-                    style_feature.view(channel, height * width).t()
-                )
-                style_loss += torch.mean((G - A) ** 2)
-
-            total_loss = alpha * original_loss + beta * style_loss
-            optimizer.zero_grad()
-            total_loss.backward()
-            optimizer.step()
-            print(step)
-
-        print(total_loss)
-        name = "Output/" + str(count) + "generated.png"
-        save_image(generated, name)
-        name = "Output/" + str(count) + "original.png"
-        save_image(original_img, name)
-        count += 1
+if __name__ ==  '__main__':
+    outputLocation="Output/"
+    if sys.argv[1]!="random":
+        if sys.argv[1] == 'cifar10':
+            trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                                     download=True, transform=transform)
+            outputLocation += "cifar10/"
+        elif sys.argv[1] == 'cifar100':
+            trainset = torchvision.datasets.CIFAR100(root='./data', train=True,
+                                                     download=True, transform=transform)
+            outputLocation += "cifar100/"
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=1,
+                                                      shuffle=True, num_workers=2)
+        perform_styles(trainloader,outputLocation)
+    else:
+        outputLocation += "random/"
+        perform_style(load_image(sys.argv[2]),outputLocation)
